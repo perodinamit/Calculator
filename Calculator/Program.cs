ed73037eb666822e4e106e5fe3078671305e035a
Matej <matej.curcic@gmail.com>
2016-03-01 18:07:00 +0100

    unos brojeva, danasnji dan

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -14,9 +14,14 @@ namespace Calculator
             Console.WriteLine("Hello Calculator");
 
             Operatori suma = new Operatori();
-            int nesto = suma.Add(5, 6);
+            Console.WriteLine("Prvi broj je:");
+            int prviBroj = int.Parse(Console.ReadLine());
+            Console.WriteLine("Drugi broj je:");
+            int drugiBroj = int.Parse(Console.ReadLine());
+            int nesto = suma.Add(prviBroj, drugiBroj);
 
-            Console.WriteLine("Rezultat je: {0}", nesto);
+            Console.WriteLine("Rezultat zbroja {0} i {1} je: {2}", prviBroj, drugiBroj, nesto);
+            Console.WriteLine("Danas je {0}", DateTime.Today.DayOfWeek);
         }
     }
 }
